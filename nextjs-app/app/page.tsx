7984ae23d4f63dde36e5714c5cbdcfa6e14bfe24
Melkis Hengue <henguemelkispro@yahoo.com>
2025-12-15 12:33:48 +0100

    fix: 🦕 Automated commit message #1765707304

---
+++ nextjs-app/app/page.tsx
@@ -0,0 +1,9 @@
+import { Navbar01 } from "@/components/ui/shadcn-io/navbar-01";
+
+export default function Home() {
+  return (
+    <div className="relative w-full">
+      <Navbar01 />
+    </div>
+  );
+}
